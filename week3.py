c109c6a0d2e0b9d5abb935b94a2fefed335a9d26
Raja Shaheryar Iftikhar <cberifti@ljmu.ac.uk>
2023-02-03 12:29:56 +0000

    task4

--- week3.py
+++ week3.py
@@ -1,5 +1,19 @@
 numbers = []
-for x in range(1,1000000):
+for x in range(1,1000001):
     numbers.append(x)
 
-print(numbers)
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+odd = []
+for y in range(1,21):
+    odd.append(y)
+print(odd)
+
+odd = [2*y  - 1 for y in range(1,11)]
+
+print(odd)
+
+for a in numbers[-3:]:
+    print(f"The first three numbers are: ", a)
